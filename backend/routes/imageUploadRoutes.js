fa5640885ab0c417a7dde1221ce8735667d88b1e
vantageGA <software@bodyvantage.co.uk>
2022-04-14 17:34:05 +0100

    routes

--- backend/routes/imageUploadRoutes.js
+++ backend/routes/imageUploadRoutes.js
@@ -35,7 +35,7 @@ const upload = multer({
 });
 
 router
-  .route('/userProfileUpload')
+  .route('/')
   .post(upload.single('userProfileImage'), protect, userProfileImageUpload);
 
 export default router;
