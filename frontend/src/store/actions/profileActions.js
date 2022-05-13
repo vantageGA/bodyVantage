c23e2d81db767643cac27e64203d5c765fb9fb4c
vantageGA <software@bodyvantage.co.uk>
2022-05-13 11:03:08 +0100

    Front end working

--- frontend/src/store/actions/profileActions.js
+++ frontend/src/store/actions/profileActions.js
@@ -19,6 +19,9 @@ import {
   PROFILE_DELETE_REVIEW_SUCCESS,
   PROFILE_DELETE_SUCCESS,
   PROFILE_FAILURE,
+  PROFILE_IMAGES_DELETE_FAILURE,
+  PROFILE_IMAGES_DELETE_REQUEST,
+  PROFILE_IMAGES_DELETE_SUCCESS,
   PROFILE_IMAGES_FAILURE,
   PROFILE_IMAGES_REQUEST,
   PROFILE_IMAGES_SUCCESS,
@@ -349,6 +352,38 @@ export const profileImagesAction = () => async (dispatch, getState) => {
   } catch (error) {
     dispatch({
       type: PROFILE_IMAGES_FAILURE,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    });
+  }
+};
+// DELETE Profile Images for ProfileImage model
+export const deleteProfileImageAction = (id) => async (dispatch, getState) => {
+  try {
+    dispatch({
+      type: PROFILE_IMAGES_DELETE_REQUEST,
+    });
+
+    const {
+      userLogin: { userInfo },
+    } = getState();
+
+    // Note: This can only be done like this in axios delete body
+    //https://stackoverflow.com/questions/51069552/axios-delete-request-with-body-and-headers
+
+    await axios.delete(`api/profile-image/${id}`, {
+      headers: {
+        Authorization: `Bearer ${userInfo.token}`,
+      },
+      data: {},
+    });
+    dispatch({ type: PROFILE_IMAGES_DELETE_SUCCESS });
+    dispatch(profileOfLoggedInUserAction());
+  } catch (error) {
+    dispatch({
+      type: PROFILE_IMAGES_DELETE_FAILURE,
       payload:
         error.response && error.response.data.message
           ? error.response.data.message
